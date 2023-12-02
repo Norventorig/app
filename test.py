7d051b0d4c5cdbb5faa88758cdbcf061ab18d5a0
Гордей Лабуз <gordlab@mail.ru>
2023-12-02 18:35:33 +1000

    Added stats and many others things (big update)

--- test.py
+++ test.py
@@ -1,90 +1,7 @@
 import random
 
 
-def input_preparation(chief_dict):
-    correct_answers = {}
-    true_values = [x for x in chief_dict.keys()][-1:-61:-1]
-    false_values = [x for x in chief_dict.keys()][-61:-241:-1]
-    random.shuffle(false_values)
-    user_keys = [chief_dict[x] for x in true_values]
-
-    for key, value in chief_dict.items():
-        correct_answers[value] = key
-
-    return correct_answers, true_values, false_values, user_keys
-
-
-def blocks_creation(true_values, false_values, user_keys):
-    mini_blocks = []
-    blocks = {}
-
-    for i_index in range(60):
-        mini_blocks.append(
-            [true_values[i_index], false_values[i_index], false_values[i_index + 1], false_values[i_index + 2]])
-        random.shuffle(mini_blocks[i_index])
-        mini_blocks[i_index].insert(0, user_keys[i_index])
-    random.shuffle(mini_blocks)
-
-    for i_list in mini_blocks:
-        blocks[i_list[0]] = (i_list[1], i_list[2], i_list[3], i_list[4])
-
-    return blocks
-
-
-def new_test(blocks, chief_dict, correct_answers):
-    errors = []
-    count = 0
-    for key, value in blocks.items():
-        count += 1
-        print("\t" * 14, key, "СЛОВО НОМЕР {}".format(count))
-        print("1){0}".format(value[0]))
-        print("2){0}".format(value[1]))
-        print("3){0}".format(value[2]))
-        print("4){0}".format(value[3]))
-        answer = input("Вариант: ")
-        print("\n" * 2)
-
-        if answer != '1' or answer != '2' or answer != '3' or answer != '4':
-            errors.append((key, correct_answers[key], ''))
-
-        elif chief_dict[value[int(answer) - 1]] != key:
-            errors.append((key, correct_answers[key], value[int(answer) - 1]))
-
-    print("\n" * 100)
-    print("+{:-^60}+{:-^60}+{:-^60}+".format('-', '-', '-'))
-    print("|{: ^60}|{: ^60}|{: ^60}|".format("Слово", "Перевод", 'Ошибка'))
-    print("+{:-^60}+{:-^60}+{:-^60}+".format('-', '-', '-'))
-    for i_error in errors:
-        print("|{: ^60}|{: ^60}|{: ^60}|".format(i_error[0], i_error[1], i_error[2]))
-    print("+{:-^60}+{:-^60}+{:-^60}+".format('-', '-', '-'))
-
-
-def standard_test(keys, dictionary):
-    errors = []
-
-    for count, word in enumerate(keys):
-        print('\n'"Слово НОМЕР {0}, ОСТАЛОСЬ {1}".format(count + 1, len(keys) - count - 1))
-        answer = input("{} -- ".format(word))
-        count += 1
-
-        if answer != dictionary[word]:
-            errors.append((word, dictionary[word], answer))
-
-    print("\n" * 100)
-    print("+{:-^60}+{:-^60}+{:-^60}+".format('-', '-', '-'))
-    print("|{: ^60}|{: ^60}|{: ^60}|".format("Слово", "Перевод", 'Ошибка'))
-    print("+{:-^60}+{:-^60}+{:-^60}+".format('-', '-', '-'))
-    for i_error in errors:
-        print("|{: ^60}|{: ^60}|{: ^60}|".format(i_error[0], i_error[1], i_error[2]))
-    print("+{:-^60}+{:-^60}+{:-^60}+".format('-', '-', '-'))
-
-
-file_opener = open("dict.txt", 'r', encoding="windows-1251")
-dictionary = file_opener.read()
-words = eval(dictionary)
-dictionary = file_opener.close()
-
-while True:
+def main_choice(words):
     print()
     print('Остановить программу (0)')
     print('120 слов (1)')
@@ -140,7 +57,168 @@ while True:
         print("\n" * 2)
 
     else:
-        break
+        file_opener = open('statistics.txt', 'r', encoding="windows-1251")
+        text = file_opener.read()
+        stat_res = text
+        text = file_opener.close()
 
-# Добавить режим отслеживания и составления статистики ошибок.
-# Добавить режим распознавания опечаток и настоящих ошибок.
+        file_opener = open('counter.txt', 'r', encoding="windows-1251")
+        text = file_opener.read()
+        count = text
+        text = file_opener.close()
+
+        print()
+        print("Статистика за последние {} использований:".format(count))
+        print(stat_res)
+
+        answer = input("Хотите очистить статистику (1 - да, enter - нет)? ")
+        if answer == '1':
+
+            file_opener = open('statistics.txt', 'w', encoding="windows-1251")
+            file_opener.write('')
+            file_opener.close()
+
+            file_opener = open('errors.txt', 'w', encoding="windows-1251")
+            file_opener.write('{}')
+            file_opener.close()
+
+            file_opener = open('counter.txt', 'w', encoding="windows-1251")
+            file_opener.write('0')
+            file_opener.close()
+
+        return
+
+    main_choice(words)
+
+
+def input_preparation(chief_dict):
+    correct_answers = {}
+    true_values = [x for x in chief_dict.keys()][-1:-61:-1]
+    false_values = [x for x in chief_dict.keys()][-61:-241:-1]
+    random.shuffle(false_values)
+    user_keys = [chief_dict[x] for x in true_values]
+
+    for key, value in chief_dict.items():
+        correct_answers[value] = key
+
+    return correct_answers, true_values, false_values, user_keys
+
+
+def blocks_creation(true_values, false_values, user_keys):
+    mini_blocks = []
+    blocks = {}
+
+    for i_index in range(60):
+        mini_blocks.append(
+            [true_values[i_index], false_values[i_index], false_values[i_index + 1], false_values[i_index + 2]])
+        random.shuffle(mini_blocks[i_index])
+        mini_blocks[i_index].insert(0, user_keys[i_index])
+    random.shuffle(mini_blocks)
+
+    for i_list in mini_blocks:
+        blocks[i_list[0]] = (i_list[1], i_list[2], i_list[3], i_list[4])
+
+    return blocks
+
+
+def new_test(blocks, chief_dict, correct_answers):
+    errors = []
+    count = 0
+
+    for key, value in blocks.items():
+        count += 1
+        print("\t" * 14, key, "СЛОВО НОМЕР {}".format(count))
+        print("1){0}".format(value[0]))
+        print("2){0}".format(value[1]))
+        print("3){0}".format(value[2]))
+        print("4){0}".format(value[3]))
+        answer = input("Вариант: ")
+        print("\n" * 2)
+
+        if answer != '1' or answer != '2' or answer != '3' or answer != '4':
+            errors.append((correct_answers[key], key, ''))
+
+        elif chief_dict[value[int(answer) - 1]] != key:
+            errors.append((correct_answers[key], key, value[int(answer) - 1]))
+
+    errors_analysis(errors)
+
+
+def standard_test(keys, dictionary):
+    errors = []
+
+    for count, word in enumerate(keys):
+        print('\n'"Слово НОМЕР {0}, ОСТАЛОСЬ {1}".format(count + 1, len(keys) - count - 1))
+        answer = input("{} -- ".format(word))
+        count += 1
+
+        if answer != dictionary[word]:
+            errors.append((dictionary[word], word, answer))
+
+    errors_analysis(errors)
+
+
+def errors_analysis(all_errors):
+    true_errors = []
+    print("\n" * 100)
+    for i_error in all_errors:
+        print("{} -- {} !- {}".format(i_error[0], i_error[1], i_error[2]), end=' ')
+        control_result = input("Действительная ошибка? (пробел - да, enter - нет): ")
+
+        if control_result == ' ':
+            true_errors.append(i_error[0])
+
+    errors_stats(true_errors)
+
+
+def errors_stats(info):
+    file_opener = open("errors.txt", 'r', encoding="windows-1251")
+    dictionary = file_opener.read()
+    text = eval(dictionary)
+    dictionary = file_opener.close()
+
+    for i_word in info:
+        if i_word in text:
+            text[i_word] += 1
+        else:
+            text[i_word] = 1
+
+    file_opener = open("errors.txt", 'w', encoding="windows-1251")
+    file_opener.write(str(text))
+    file_opener.close()
+
+    stats(text)
+
+
+def stats(info, count=1):
+    error_counter = 0
+    statistic = ''
+
+    for i_count in info.values():
+        error_counter += i_count
+
+    unprepared_stats = [[i_word, round(i_count / error_counter * 100, 2)] for i_word, i_count in info.items()]
+    unprepared_stats.sort()
+
+    for i_stat in unprepared_stats:
+        statistic += '"{}" - {}%\n'.format(i_stat[0], i_stat[1])
+
+    file_opener = open('statistics.txt', 'w', encoding="windows-1251")
+    file_opener.write(statistic)
+    file_opener.close()
+
+    file_opener = open("counter.txt", 'r', encoding="windows-1251")
+    text = file_opener.read()
+    previous_count = int(text)
+    text = file_opener.close()
+
+    file_opener = open('counter.txt', 'w', encoding="windows-1251")
+    file_opener.write(str(previous_count + count))
+    file_opener.close()
+
+
+file_opener = open("dict.txt", 'r', encoding="windows-1251")
+text = file_opener.read()
+dictionary = eval(text)
+text = file_opener.close()
+main_choice(dictionary)
