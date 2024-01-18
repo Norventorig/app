7f1dc7265da718ed61195a4bde77dd87e765f992
Norvent <gordlab@mail.ru>
2024-01-18 18:00:32 +1000

    implemented correction of mistakes

--- test.py
+++ test.py
@@ -60,7 +60,7 @@ def main_choice(words, special_words):
         file_opener.close()
 
     else:
-        with open('conter.txt', 'r', encoding='windows-1251') as counter_reader:
+        with open('counter.txt', 'r', encoding='windows-1251') as counter_reader:
             count = counter_reader.read()
 
         with open('statistics.txt', 'r', encoding="windows-1251") as statistic_reader:
@@ -68,18 +68,6 @@ def main_choice(words, special_words):
 
         print("Статистика за последние {} использований:".format(count))
         print(stat_res)
-
-        answer = input("Хотите очистить статистику (1 - да, enter - нет)? ")
-        if answer == '1':
-
-            with open('statistics.txt', 'w', encoding="UTF-8") as statistic_reset:
-                statistic_reset.write('')
-
-            with open('errors.txt', 'w', encoding="UTF-8") as errors_reset:
-                errors_reset.write('{}')
-
-            with open('counter.txt', 'w', encoding="UTF-8") as counter_reset:
-                counter_reset.write("0")
 
         return
 
@@ -97,11 +85,43 @@ def standard_test(keys, dictionary):
         if answer != dictionary[word]:
             errors.append((dictionary[word], word, answer))
 
-    with open('counter.txt', 'r+', encoding='UTF-8') as counter_adder:
-        count = int(counter_adder.read())
-        counter_adder.write(str(count + 1))
+    print('\n', ' '*83, 'ОШИБКИ:', ' '*83, '\n')
+    for i_error in errors:
+        print("{} == {} !=! {}".format(i_error[1], i_error[0], i_error[2]))
+
+    with open('counter.txt', 'r', encoding='UTF-8') as counter_reader:
+        count = int(counter_reader.read())
+        count += 1
+
+    with open('counter.txt', 'w', encoding='UTF-8') as counter_adder:
+        counter_adder.write(str(count))
 
     errors_stats(errors)
+
+    if count == 11:
+        errors_work(dictionary)
+
+
+def errors_work(dictionary):
+    with open("errors.txt", 'r', encoding='UTF-8') as errors_reader:
+        errors_values = (eval(errors_reader.read())).keys()
+        errors_keys = []
+
+    for i_key, i_value in dictionary.items():
+
+        if i_value in errors_values:
+            errors_keys.append(i_key)
+
+    with open('statistics.txt', 'w', encoding="UTF-8") as statistic_reset:
+        statistic_reset.write('')
+
+    with open('errors.txt', 'w', encoding="UTF-8") as errors_reset:
+        errors_reset.write('{}')
+
+    with open('counter.txt', 'w', encoding="UTF-8") as counter_reset:
+        counter_reset.write("0")
+
+    standard_test(errors_keys, dictionary)
 
 
 def errors_stats(errors_list):
@@ -111,10 +131,10 @@ def errors_stats(errors_list):
     for i in errors_list:
 
         if i[1] in dict_errors.keys():
-            dict_errors[i[1]] += 1
+            dict_errors[i[0]] += 1
 
         else:
-            dict_errors[i[1]] = 1
+            dict_errors[i[0]] = 1
 
     stats(dict_errors)
 
@@ -129,8 +149,11 @@ def stats(errors_amount):
     for i_error, i_amount in errors_amount.items():
         statistic[i_error] = "{}".format(round((i_amount / errors_counter) * 100, 1))
 
-    with open("statistic.txt", 'w', encoding="UTF-8") as statistic_writer:
+    with open("statistics.txt", 'w', encoding="UTF-8") as statistic_writer:
         statistic_writer.write(str(statistic))
+
+    with open('errors.txt', 'w', encoding="UTF-8") as errors_writer:
+        errors_writer.write(str(errors_amount))
 
 
 with open("dict.txt", 'r', encoding="windows-1251") as file_reader:
@@ -140,3 +163,4 @@ with open("Резервный файл.txt", 'r', encoding="windows-1251") as fi
     special_dictionary = eval(file_reader.read())
 
 main_choice(main_dictionary, special_dictionary)
+3
